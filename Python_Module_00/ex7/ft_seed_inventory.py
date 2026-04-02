7899c4485993bc46d6f12bd5658e05e7f1e98003
Daniel Cheng <dcheng@u81z04s03.42kl.edu.my>
2026-04-02 15:59:04 +0800

    stuff

--- Python_Module_00/ex7/ft_seed_inventory.py
+++ Python_Module_00/ex7/ft_seed_inventory.py
@@ -1,9 +1,9 @@
-def ft_seed_inventory(seed: str, count: int, unit: str) -> None:
+def ft_seed_inventory(seed: str, quantity: int, unit: str) -> None:
     if unit == "packets":
-        print(seed.capitalize(), "seeds:", count, unit, "available")
+        print(seed.capitalize(), "seeds:", quantity, unit, "available")
     elif unit == "grams":
-        print(seed.capitalize(), "seeds:", count, unit, "total")
+        print(seed.capitalize(), "seeds:", quantity, unit, "total")
     elif unit == "area":
-        print(seed.capitalize(), "seeds: covers", count, "square meters")
+        print(seed.capitalize(), "seeds: covers", quantity, "square meters")
     else:
         print("Unknown unit type")
