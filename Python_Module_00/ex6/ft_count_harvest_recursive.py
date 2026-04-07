e0322fbfdec833c78f53b134d9f21d2559542f5e
Daniel Cheng <dcheng@u90z08s01.42kl.edu.my>
2026-04-07 11:55:02 +0800

    stuff

--- Python_Module_00/ex6/ft_count_harvest_recursive.py
+++ Python_Module_00/ex6/ft_count_harvest_recursive.py
@@ -1,4 +1,4 @@
-def ft_count_harvest_recursive(days: int = 1, n: int = None):
+def ft_count_harvest_recursive(days: int = 1, n: int = False):
     n = n or int(input("Days until harvest: "))
     print(f"Day {days}" if days <= n else
           "Harvest time!") or (days <= n and ft_count_harvest_recursive(days +
